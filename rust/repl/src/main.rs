a083a31ee14afa36c5cb50b200025cb531db7d23
JingusJohn <branchjohnchristopher@gmail.com>
2024-01-26 10:21:01 -0600

    added repl and restructured lexer

---
+++ rust/repl/src/main.rs
@@ -0,0 +1,32 @@
+use lexer::tokens::Token;
+use lexer::Lexer;
+use std::io::{stdin, stdout, Write};
+
+fn main() {
+    println!("Welcome to the Monkey-Lang REPL");
+    loop {
+        let mut input = String::new();
+        print!(">> ");
+        let _ = stdout().flush();
+        stdin().read_line(&mut input).expect("Input error!");
+        if let Some('\n') = input.chars().next_back() {
+            input.pop();
+        }
+        if let Some('\r') = input.chars().next_back() {
+            input.pop();
+        }
+        println!("you entered: {}", input);
+
+        let mut tok = Lexer::new(input);
+        loop {
+            match tok.next_token() {
+                Token::EOF => {
+                    break;
+                }
+                a => {
+                    println!("{:?}", a);
+                }
+            }
+        }
+    }
+}
